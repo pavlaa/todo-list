43b200b71c79a097c7880d676c952c8468477c9d
Pavel Gritsenko <pdudarevp@gmail.com>
2022-03-10 15:20:17 +0300

    feat: add task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.scss';
 import search from './img/search.svg'
+import deleteImg from './img/delete.svg'
+import done1 from './img/check1.svg'
+import done2 from './img/check2.svg'
 
 function App() {
   return (
@@ -15,6 +18,22 @@ function App() {
             </div>
           </div>
           <div className="action__create">+</div>
+        </div>
+        <div className="todo__task task">
+          <div className="task__desc">
+            <div className="task__id">1.</div>
+            <div className="task__text">
+              Flexbox - это целый модуль, содержащий в себе набор свойств и их значений
+            </div>
+          </div>
+          <div className="task__btn">
+            <div className="task__complete">
+              <img src={done1} alt="done-btn"/>
+            </div>
+            <div className="task__delete">
+              <img src={deleteImg} alt="delete-btn"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
